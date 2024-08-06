f2717736086a16bbcdd0745a8ecfeb9f1b313ce0
lentil32 <lentil32@icloud.com>
2024-08-06 16:46:41 +0900

    Update zsh system-wide config

--- modules/system.nix
+++ modules/system.nix
@@ -158,7 +158,12 @@
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    loginShellInit = ''
+      ulimit -Sn 10240
+    '';
+  };
   environment.shells = [ pkgs.zsh ];
 
   # GPG
