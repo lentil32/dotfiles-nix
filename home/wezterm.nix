27d7edccc3bc86e849314ba462a52cbda9503bc3
lentil32 <lentil32@icloud.com>
2026-01-07 16:32:35 -0800

    chore: Add shift-enter handling for wezterm

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -10,6 +10,9 @@
       config.color_scheme = 'Tokyo Night'
       config.font = wezterm.font 'Iosevka'
       config.font_size = 14.0
+      config.keys = {
+        { key = 'Enter', mods = 'SHIFT', action = wezterm.action { SendString = '\x1b\r' } },
+      }
 
       return config
     '';
