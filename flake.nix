66870a400fa44fe98b15a11a516330d5f0b78eb3
lentil32 <lentil32@icloud.com>
2026-02-05 00:08:33 +0900

    chore: move caches to nix settings

--- flake.nix
+++ flake.nix
@@ -1,17 +1,6 @@
 {
   description = "Declarative macOS configuration with nix-darwin + Home Manager";
 
-  nixConfig = {
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -6,6 +6,16 @@
     "flakes"
   ];
 
+  # Enable trusted binary caches globally (not just per-flake).
+  nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
